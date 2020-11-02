246deeed6bbb33f082d793ffb069e867dd9c738d
Juliana sabrina Herrera <66982758+herrerajuli@users.noreply.github.com>
2020-11-02 14:13:11 -0500

    RutaAdmi.js cree esta pantalla

---
+++ frontend/src/components/RutaAdmi.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Container } from 'react-bootstrap';
+import '../style/RutaAdmi.css'
+import { withRouter } from "react-router-dom";
+
+const RutaAdmi = (props) => {
+  const { history } = props;
+  return(
+<div className="RutaAdmi">
+<Container className="text-center" >
+<form className="form-signin">
+<h1 class="h3 mb-3 font-weight-normal">Ruta ambiental</h1>
+<br/>
+<br/>
+
+<img class="mb-4" src="./logo1...png" alt="" width="112" height="92"/>
+<br/>
+<br/>
+Responde diversas preguntas y gana puntajes, logros que te ayudaran con el desarrollo de tu conocimiento
+
+<br/>
+  <br/>
+  <br/>
+  <button class="btn btn-lg btn-primary btn-block" type="submit" onClick={() => history.push("/PreguntasAdmi")}>Jugar</button>
+  
+  
+</form>
+</Container>
+</div>
+  );
+};
+export default withRouter(RutaAdmi);
