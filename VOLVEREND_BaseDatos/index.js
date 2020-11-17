ebcc4fd0f8990ba06cfb3c92c7042c18539d516e
Maria Arends <66528316+Marends0416@users.noreply.github.com>
2020-11-17 01:51:31 -0500

    index.js

---
+++ VOLVEREND_BaseDatos/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+// definir todos los componentes, modulos app
+const routes= require('./routes/Usuario');// crud
+const modulos = require('./routes/material');// crud
+const colegios = require('./routes/ruta_ambiental');// crud
+const colegios = require('./routes/Navegar');// crud
+const colegios = require('./routes/consejo');// crud
+const colegios = require('./routes/Pregunta');// crud
+const colegios = require('./routes/Pregunta_logro');// crud
+const colegios = require('./routes/logro');// crud
+//modificar const 
+//ajustes
+app.set('port',3000) 
+// Middleware
+app.use(express.json()); 
+// ajustess
+app.use('/api',Usuario);
+app.use('/api/material',material);
+app.use('/api/ruta_ambiental',ruta_ambiental);
+app.use('/api/Navegar',Navegar);
+app.use('/api/consejo',consejo);
+app.use('/api/Pregunta',Pregunta);
+app.use('/api/Pregunta_logro',Pregunta_logro);
+app.use('/api/logro',logro);
+//modificar api 
+app.listen(app.get('port'),()=> {
+    console.log(`Servidor corriendo en el puerto ${app.get('port')}`);
+    });
+
+//Ajustes
+app.set('port',3000);
+
+//Middlewares
+app.use(express.json());
+
+//Ajustes del servidor
+app.use('/api',routes);
+app.listen(app.get('port'),()=> {
+console.log(`Servidor corriendo en el puerto ${app.get('port')}`);
+});
