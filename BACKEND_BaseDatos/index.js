c6a7035e76e2b7d594dd5038938d226f57753138
Marends0416 <Skarelop026@gmail.com>
2020-11-17 09:27:35 -0500

    first commit

--- BACKEND_BaseDatos/index.js
+++ BACKEND_BaseDatos/index.js
@@ -1,15 +1,30 @@
-const express = require('express');
-const app = express();
-const routes = require('./routes/routes');
-
-//Ajustes
-app.set('port',3000);
-
-//Middlewares
-app.use(express.json());
-
-//Ajustes del servidor
-app.use('/api',routes);
+const express= require('express');
+const app = express(); 
+// definir todos los componentes, modulos app
+const routes= require('./routes/Usuario');// crud
+const modulos = require('./routes/material');// crud
+const colegios = require('./routes/ruta_ambiental');// crud
+const colegios = require('./routes/Navegar');// crud
+const colegios = require('./routes/consejo');// crud
+const colegios = require('./routes/Pregunta');// crud
+const colegios = require('./routes/Pregunta_logro');// crud
+const colegios = require('./routes/logro');// crud
+//modificar const 
+//ajustes
+app.set('port',3000) 
+// Middleware
+app.use(express.json()); 
+// ajustess
+app.use('/api',Usuario);
+app.use('/api/material',material);
+app.use('/api/ruta_ambiental',ruta_ambiental);
+app.use('/api/Navegar',Navegar);
+app.use('/api/consejo',consejo);
+app.use('/api/Pregunta',Pregunta);
+app.use('/api/Pregunta_logro',Pregunta_logro);
+app.use('/api/logro',logro);
+//modificar api 
 app.listen(app.get('port'),()=> {
-console.log(`Servidor corriendo en el puerto ${app.get('port')}`);
-});
+    console.log(`Servidor corriendo en el puerto ${app.get('port')}`);
+    });
+
---
+++ BACKEND_BaseDatos/routes/material.js
@@ -0,0 +1,86 @@
+const { Router } = require('express');
+const router = Router();
+const fs = require('fs');
+
+
+const rutinasFile = fs.readFileSync("./rutinas.json", "utf-8");
+let rutinas = JSON.parse(rutinasFile);
+
+router.get("/", (req, res) => {
+    res.json("API-JSON-CRUD POLIJIC");
+});
+router.get("/rutinas", (req, res) => {
+    res.status(200).json(rutinas);
+
+});
+
+router.post("/ejercicio", (req, res) => {
+    const { titulo, tiempo, estimado, zona, ejercicio, video } = req.body;
+    if (!titulo || !tiempo || !estimado || !zona || !ejercicio || !video) {
+ 
+        res.status(4001).json({ error: "Debe completar todos los campos" });
+        console.log("Debe completar todos los campos");
+    } else {
+      
+        const id = rutinas.length + 1;
+        let newrutinas = {
+            id, titulo, tiempo, estimado, zona, ejercicio, video
+        }
+
+        rutinas.push(newrutinas);
+        const json_rutinas = JSON.stringify(rutinas);
+        fs.writeFileSync("./rutinas.json", json_rutinas, "utf-8");
+        res.status(200).json(rutinas);
+    }
+}); 
+router.put("/ejercicio/:id", (req, res) => {
+    const { titulo, tiempo, estimado, zona, ejercicio, video } = req.body;
+    const id = req.params.id;
+    if (!titulo || !tiempo || !estimado || !zona || !ejercicio || !video || !id) {
+        res.status(401).json({ error: " Debes completar todos los campos " });
+    }
+    else {
+        rutinas.filter((rutinas) => {
+            if (rutinas.id == id) { 
+                rutinas.titulo = titulo;
+                rutinas.tiempo = tiempo;
+                rutinas.estimado = estimado;
+                rutinas.zona = zona;
+                rutinas.ejercicio = ejercicio;
+                rutinas.video = video;
+            }
+
+        }); 
+        const json_rutinas = JSON.stringify(rutinas);
+        fs.writeFileSync("./rutinas.json", json_rutinas, "utf-8");
+        res.status(200).json(rutinas);
+    }
+    });
+
+
+
+    router.delete("/rutinas/:id", (req, res) => {
+        const id = req.params.id;
+    
+        if (!id) {
+            res
+                .status(401)
+                .json({ error: "Ingrese el código de la pelicula a eliminar" });
+            } else {
+                const indexrutinas = rutinas.findIndex((rutinas) => rutinas.id === id);
+               
+                rutinas.splice(indexrutinas, 1);
+                const json_rutinas = JSON.stringify(rutinas);
+                fs.writeFileSync("./rutinas.json", json_rutinas, "utf-8");
+                res.status(200).json(rutinas);
+            }
+    
+        
+    
+    
+    
+    
+    }); 
+    
+    
+    module.exports = router;
