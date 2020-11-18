90ce281e6e034cfd3b49f122a0dd4f5d6c7375ed
Maria Arends <66528316+Marends0416@users.noreply.github.com>
2020-11-17 23:38:10 -0500

    Update routes.js

--- BACKEND_BaseDatos/routes/routes.js
+++ BACKEND_BaseDatos/routes/routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const mysqlConnection = require('../db/db');
 
 router.get('/Usuario ',(req,res)=>{
-mysqlConnection.Query('SELECT * FROM nombre ',(err, rows, fields) =>{
+mysqlConnection.Query('SELECT * FROM Usuario ',(err, rows, fields) =>{
     if(!err){
         res.json(rows);
     } else {
